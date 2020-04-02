f68e3546a37da5ec1c4778713cd6ebe6917daede
Emily Bache <emily.bache@praqma.com>
2020-04-02 08:42:40 +0200

    rename to match master

--- cpp/alarm_clock.c
+++ cpp/alarm_clock.c
@@ -2,35 +2,35 @@
 #include "alarm_clock.h"
 
 void
-how_long_until_the_next_alarm(struct alarm_config *alarmConfig, const unsigned int now_sec,
-                              unsigned long *min_value_ms) {
+how_long_until_next_timer_expiry(struct timer_config *timerConfig, const unsigned int now_sec,
+                                 unsigned long *min_value_ms) {
     unsigned int time_sec;
     unsigned int diff_sec;
     unsigned int min_value_sec = INT_MAX;
 
-    unsigned int op_flags = get_operational_flags(alarmConfig);
-    unsigned int reporting_flags = get_reporting_flags(alarmConfig);
-    unsigned int last_pkt_diff = now_sec - get_time_of_last_pkt(alarmConfig);
+    unsigned int op_flags = get_operational_flags(timerConfig);
+    unsigned int reporting_flags = get_reporting_flags(timerConfig);
+    unsigned int last_pkt_diff = now_sec - get_time_of_last_pkt(timerConfig);
 
-    if (duration_measurement_active(alarmConfig)) {
-        unsigned int pkt_rx_diff = now_sec - get_duration_meas_start(alarmConfig);
+    if (duration_measurement_active(timerConfig)) {
+        unsigned int pkt_rx_diff = now_sec - get_duration_meas_start(timerConfig);
 
-        if (get_idt_alarm_time(alarmConfig) != 0) {
-            time_sec = get_idt_alarm_time(alarmConfig);
+        if (get_idt_alarm_time(timerConfig) != 0) {
+            time_sec = get_idt_alarm_time(timerConfig);
             if ((time_sec - last_pkt_diff) < min_value_sec) {
                 min_value_sec = time_sec - last_pkt_diff;
             }
         }
-        if ((reporting_flags & REPORTING_TRIGGERS_P88N) && (get_time_threshold(alarmConfig) != 0)) {
-            time_sec = get_time_threshold(alarmConfig) -
-                       ((get_duration_meas(alarmConfig) + get_duration_meas_threshold_used(alarmConfig))
+        if ((reporting_flags & REPORTING_TRIGGERS_P88N) && (get_time_threshold(timerConfig) != 0)) {
+            time_sec = get_time_threshold(timerConfig) -
+                       ((get_duration_meas(timerConfig) + get_duration_meas_threshold_used(timerConfig))
                         + pkt_rx_diff);
             if (time_sec < min_value_sec) {
                 min_value_sec = time_sec;
             }
         }
-        if ((op_flags & OPERATIONAL_FLAG_TIME_QUOTA_PRESENT) && (get_time_quota(alarmConfig) != 0)) {
-            time_sec = get_time_quota(alarmConfig) - (get_duration_meas(alarmConfig) + pkt_rx_diff);
+        if ((op_flags & OPERATIONAL_FLAG_TIME_QUOTA_PRESENT) && (get_time_quota(timerConfig) != 0)) {
+            time_sec = get_time_quota(timerConfig) - (get_duration_meas(timerConfig) + pkt_rx_diff);
             if (time_sec < min_value_sec) {
                 min_value_sec = time_sec;
             }
@@ -38,15 +38,15 @@ how_long_until_the_next_alarm(struct alarm_config *alarmConfig, const unsigned i
     }
 
     if (reporting_flags & REPORTING_TRIGGERS_ZB12) {
-        if ((get_quota_holding_time(alarmConfig) != 0) &&
-            !get_operational_flag_state(alarmConfig, OPERATIONAL_FLAG_ZB12_STOPPED)) {
+        if ((get_quota_holding_time(timerConfig) != 0) &&
+            !get_operational_flag_state(timerConfig, OPERATIONAL_FLAG_ZB12_STOPPED)) {
             /* If ZB12 is just provisioned, start timer with provisioned value
              * If ZB12 is modified, start timer with modified value and reset ZB12 modified flag
              * If ZB12 is not modified (and not just provisioned), restart the session timer for remainder of the provisioned value
              */
-            time_sec = get_quota_holding_time(alarmConfig);
-            if (!test_and_clear_op_flag(alarmConfig, OPERATIONAL_FLAG_ZB12_MODIFIED) &&
-                (get_time_of_last_pkt(alarmConfig) != 0)) {
+            time_sec = get_quota_holding_time(timerConfig);
+            if (!test_and_clear_op_flag(timerConfig, OPERATIONAL_FLAG_ZB12_MODIFIED) &&
+                (get_time_of_last_pkt(timerConfig) != 0)) {
                 time_sec -= last_pkt_diff;
             }
 
@@ -56,17 +56,17 @@ how_long_until_the_next_alarm(struct alarm_config *alarmConfig, const unsigned i
         }
     }
 
-    if ((reporting_flags & REPORTING_TRIGGERS_DY9X) && (get_meas_DY9Xd(alarmConfig) != 0)) {
-        diff_sec = now_sec - get_periodic_meas_start(alarmConfig);
-        time_sec = get_meas_DY9Xd(alarmConfig) - diff_sec;
+    if ((reporting_flags & REPORTING_TRIGGERS_DY9X) && (get_meas_DY9Xd(timerConfig) != 0)) {
+        diff_sec = now_sec - get_periodic_meas_start(timerConfig);
+        time_sec = get_meas_DY9Xd(timerConfig) - diff_sec;
         if (time_sec < min_value_sec) {
             min_value_sec = time_sec;
         }
     }
     // Handle timer for Monitoring Time
-    if (get_monitoring_time_ts(alarmConfig) != 0) {
-        diff_sec = now_sec - get_monitoring_time_start(alarmConfig);
-        time_sec = get_monitoring_time_ts(alarmConfig) - diff_sec;
+    if (get_monitoring_time_ts(timerConfig) != 0) {
+        diff_sec = now_sec - get_monitoring_time_start(timerConfig);
+        time_sec = get_monitoring_time_ts(timerConfig) - diff_sec;
         if (time_sec < min_value_sec) {
             min_value_sec = time_sec;
         }
@@ -79,147 +79,147 @@ how_long_until_the_next_alarm(struct alarm_config *alarmConfig, const unsigned i
     }
 }
 
-void clear_operational_flag(struct alarm_config *pAlarmConfig,
+void clear_operational_flag(struct timer_config *timerConfig,
                             unsigned int flag) {
-    pAlarmConfig->operational_flags &= ~flag;
+    timerConfig->operational_flags &= ~flag;
 }
 
-bool test_and_clear_op_flag(struct alarm_config *pAlarmConfig, unsigned int flag) {
-    bool result = get_operational_flag_state(pAlarmConfig, flag);
+bool test_and_clear_op_flag(struct timer_config *timerConfig, unsigned int flag) {
+    bool result = get_operational_flag_state(timerConfig, flag);
     if (result)
-        clear_operational_flag(pAlarmConfig, flag);
+        clear_operational_flag(timerConfig, flag);
     return result;
 }
 
-bool get_operational_flag_state(const struct alarm_config *pAlarmConfig, unsigned int flag) {
-    return pAlarmConfig->operational_flags & flag;
+bool get_operational_flag_state(const struct timer_config *timerConfig, unsigned int flag) {
+    return timerConfig->operational_flags & flag;
 }
 
-unsigned int get_monitoring_time_start(const struct alarm_config *pAlarmConfig) {
-    return pAlarmConfig->timers->monitoring_time_start;
+unsigned int get_monitoring_time_start(const struct timer_config *timerConfig) {
+    return timerConfig->timers->monitoring_time_start;
 }
 
-unsigned int get_monitoring_time_ts(const struct alarm_config *pAlarmConfig) {
-    return pAlarmConfig->timers->monitoring_time_ts;
+unsigned int get_monitoring_time_ts(const struct timer_config *timerConfig) {
+    return timerConfig->timers->monitoring_time_ts;
 }
 
-unsigned int get_periodic_meas_start(const struct alarm_config *pAlarmConfig) {
-    return pAlarmConfig->timers->periodic_meas_start;
+unsigned int get_periodic_meas_start(const struct timer_config *timerConfig) {
+    return timerConfig->timers->periodic_meas_start;
 }
 
-unsigned int get_meas_DY9Xd(const struct alarm_config *pAlarmConfig) {
-    return pAlarmConfig->timers->meas_dy9xd;
+unsigned int get_meas_DY9Xd(const struct timer_config *timerConfig) {
+    return timerConfig->timers->meas_dy9xd;
 }
 
-unsigned int get_quota_holding_time(const struct alarm_config *pAlarmConfig) {
-    return pAlarmConfig->timers->quota_holding_time;
+unsigned int get_quota_holding_time(const struct timer_config *timerConfig) {
+    return timerConfig->timers->quota_holding_time;
 }
 
-unsigned int get_time_quota(const struct alarm_config *pAlarmConfig) {
-    return pAlarmConfig->time_quota;
+unsigned int get_time_quota(const struct timer_config *timerConfig) {
+    return timerConfig->time_quota;
 }
 
-unsigned int get_duration_meas_threshold_used(const struct alarm_config *pAlarmConfig) {
-    return pAlarmConfig->timers->duration->meas_threshold_used;
+unsigned int get_duration_meas_threshold_used(const struct timer_config *timerConfig) {
+    return timerConfig->timers->duration->meas_threshold_used;
 }
 
-unsigned int get_duration_meas(const struct alarm_config *pAlarmConfig) {
-    return pAlarmConfig->timers->duration->meas;
+unsigned int get_duration_meas(const struct timer_config *timerConfig) {
+    return timerConfig->timers->duration->meas;
 }
 
-unsigned int get_time_threshold(const struct alarm_config *pAlarmConfig) {
-    return pAlarmConfig->time_threshold;
+unsigned int get_time_threshold(const struct timer_config *timerConfig) {
+    return timerConfig->time_threshold;
 }
 
-unsigned int get_idt_alarm_time(const struct alarm_config *pAlarmConfig) {
-    return pAlarmConfig->idt_alarm_time;
+unsigned int get_idt_alarm_time(const struct timer_config *timerConfig) {
+    return timerConfig->idt_alarm_time;
 }
 
-unsigned int get_duration_meas_start(const struct alarm_config *pAlarmConfig) {
-    return pAlarmConfig->timers->duration->meas_start;
+unsigned int get_duration_meas_start(const struct timer_config *timerConfig) {
+    return timerConfig->timers->duration->meas_start;
 }
 
-unsigned int get_reporting_flags(const struct alarm_config *pAlarmConfig) {
-    return pAlarmConfig->reporting_flags;
+unsigned int get_reporting_flags(const struct timer_config *timerConfig) {
+    return timerConfig->reporting_flags;
 }
 
-unsigned int get_time_of_last_pkt(const struct alarm_config *pAlarmConfig) {
-    return pAlarmConfig->last_pkt;
+unsigned int get_time_of_last_pkt(const struct timer_config *timerConfig) {
+    return timerConfig->last_pkt;
 }
 
-bool duration_measurement_active(const struct alarm_config *pAlarmConfig) {
-    return pAlarmConfig->timers->duration->meas_active;
+bool duration_measurement_active(const struct timer_config *timerConfig) {
+    return timerConfig->timers->duration->meas_active;
 }
 
-unsigned int get_operational_flags(const struct alarm_config *pAlarmConfig) {
-    return pAlarmConfig->operational_flags;
+unsigned int get_operational_flags(const struct timer_config *timerConfig) {
+    return timerConfig->operational_flags;
 }
 
-unsigned int get_bti_time_interval(const struct alarm_config *config) {
+unsigned int get_bti_time_interval(const struct timer_config *config) {
     return config->timers->duration->bti_time_interval;
 }
 
-void set_duration_meas_active(struct alarm_config *config, bool value) {
-    config->timers->duration->meas_active = true;
+void set_duration_meas_active(struct timer_config *config, bool value) {
+    config->timers->duration->meas_active = value;
 }
 
-void set_duration_meas_start(struct alarm_config *config, unsigned int value) {
+void set_duration_meas_start(struct timer_config *config, unsigned int value) {
     config->timers->duration->meas_start = value;
 }
 
-void set_idt_alarm_time(struct alarm_config *config, unsigned int value) {
+void set_idt_alarm_time(struct timer_config *config, unsigned int value) {
     config->idt_alarm_time = value;
 }
 
-void set_last_pkt_time(struct alarm_config *config, unsigned int value) {
+void set_last_pkt_time(struct timer_config *config, unsigned int value) {
     config->last_pkt = value;
 }
 
-void add_reporting_flag(struct alarm_config *config, unsigned int flag) {
+void add_reporting_flag(struct timer_config *config, unsigned int flag) {
     config->reporting_flags += flag;
 }
 
-void set_time_threshold(struct alarm_config *config, unsigned int value) {
+void set_time_threshold(struct timer_config *config, unsigned int value) {
     config->time_threshold = value;
 }
 
-void set_duration_meas_threshold(struct alarm_config *config, unsigned int value) {
+void set_duration_meas_threshold(struct timer_config *config, unsigned int value) {
     config->timers->duration->meas_threshold_used = value;
 }
 
-void add_operational_flag(struct alarm_config *config, unsigned int flag) {
+void add_operational_flag(struct timer_config *config, unsigned int flag) {
     config->operational_flags += flag;
 }
 
-void set_time_quota(struct alarm_config *config, unsigned int value) {
+void set_time_quota(struct timer_config *config, unsigned int value) {
     config->time_quota = value;
 }
 
-void set_duration_meas(struct alarm_config *config, unsigned int value) {
+void set_duration_meas(struct timer_config *config, unsigned int value) {
     config->timers->duration->meas = value;
 }
 
-void set_quota_holding_time(struct alarm_config *config, unsigned int value) {
+void set_quota_holding_time(struct timer_config *config, unsigned int value) {
     config->timers->quota_holding_time = value;
 }
 
-void set_meas_dy9xd(struct alarm_config *config, unsigned int value) {
+void set_meas_dy9xd(struct timer_config *config, unsigned int value) {
     config->timers->meas_dy9xd = value;
 }
 
-void set_periodic_meas_start(struct alarm_config *config, unsigned int value) {
+void set_periodic_meas_start(struct timer_config *config, unsigned int value) {
     config->timers->periodic_meas_start = value;
 }
 
-void set_monitoring_time_ts(struct alarm_config *config, unsigned int value) {
+void set_monitoring_time_ts(struct timer_config *config, unsigned int value) {
     config->timers->monitoring_time_ts = value;
 }
 
-void set_monitoring_time_start(struct alarm_config *config, unsigned int value) {
+void set_monitoring_time_start(struct timer_config *config, unsigned int value) {
     config->timers->monitoring_time_start = value;
 }
 
-void set_bti_time_interval(struct alarm_config *config, unsigned int value) {
+void set_bti_time_interval(struct timer_config *config, unsigned int value) {
     config->timers->duration->bti_time_interval = value;
 }
 
--- cpp/tests/alarm_clock_test.cpp
+++ cpp/tests/alarm_clock_test.cpp
@@ -9,17 +9,17 @@ extern "C"
 #include "../alarm_clock.h"
 }
 
-TEST_CASE ("how_long_until_the_next_alarm") {
+TEST_CASE ("how_long_until_next_timer_expiry") {
 
     unsigned int now_sec = 100; // seconds since the epoch
     unsigned long min_value_ms = LONG_MAX; // when the next timer should go off, in milliseconds after now_sec
 
-    auto *alarmConfig = new alarm_config();
-    alarmConfig->timers = new timers();
-    alarmConfig->timers->duration = new duration();
+    auto *timerConfig = new timer_config();
+    timerConfig->timers = new timers();
+    timerConfig->timers->duration = new duration();
 
-    SECTION("no alarms set") {
-        how_long_until_the_next_alarm(alarmConfig, now_sec, &min_value_ms);
+    SECTION("no timers set") {
+        how_long_until_next_timer_expiry(timerConfig, now_sec, &min_value_ms);
 
         // no timers are set, so min_value_ms is not updated and remains at the value we set earlier
         REQUIRE(min_value_ms == LONG_MAX);
@@ -27,87 +27,87 @@ TEST_CASE ("how_long_until_the_next_alarm") {
 
     SECTION("duration measurement active")
     {
-        set_duration_meas_active(alarmConfig, true);
-        set_duration_meas(alarmConfig, 1);
-        set_duration_meas_start(alarmConfig, now_sec - 1);
-        set_last_pkt_time(alarmConfig, now_sec);
+        set_duration_meas_active(timerConfig, true);
+        set_duration_meas(timerConfig, 1);
+        set_duration_meas_start(timerConfig, now_sec - 1);
+        set_last_pkt_time(timerConfig, now_sec);
         SECTION("idt alarm") {
-            set_idt_alarm_time(alarmConfig, 3);
-            how_long_until_the_next_alarm(alarmConfig, now_sec, &min_value_ms);
+            set_idt_alarm_time(timerConfig, 3);
+            how_long_until_next_timer_expiry(timerConfig, now_sec, &min_value_ms);
             REQUIRE(min_value_ms == 3000);
         }
-        SECTION("P88N alarm") {
-            add_reporting_flag(alarmConfig, REPORTING_TRIGGERS_P88N);
-            set_time_threshold(alarmConfig, 10);
-            how_long_until_the_next_alarm(alarmConfig, now_sec, &min_value_ms);
+        SECTION("P88N timer") {
+            add_reporting_flag(timerConfig, REPORTING_TRIGGERS_P88N);
+            set_time_threshold(timerConfig, 10);
+            how_long_until_next_timer_expiry(timerConfig, now_sec, &min_value_ms);
             REQUIRE(min_value_ms == 8000);
         }
-        SECTION("Time Quota alarm") {
-            add_operational_flag(alarmConfig, OPERATIONAL_FLAG_TIME_QUOTA_PRESENT);
-            set_time_quota(alarmConfig, 5);
-            how_long_until_the_next_alarm(alarmConfig, now_sec, &min_value_ms);
+        SECTION("Time Quota timer") {
+            add_operational_flag(timerConfig, OPERATIONAL_FLAG_TIME_QUOTA_PRESENT);
+            set_time_quota(timerConfig, 5);
+            how_long_until_next_timer_expiry(timerConfig, now_sec, &min_value_ms);
             REQUIRE(min_value_ms == 3000);
         }
     }
 
-    SECTION("ZB12") {
-        add_reporting_flag(alarmConfig, REPORTING_TRIGGERS_ZB12);
-        set_quota_holding_time(alarmConfig, 6);
-        clear_operational_flag(alarmConfig, OPERATIONAL_FLAG_ZB12_STOPPED); // this should be the default anyway
-        set_last_pkt_time(alarmConfig, now_sec);
+    SECTION("ZB12 timer") {
+        add_reporting_flag(timerConfig, REPORTING_TRIGGERS_ZB12);
+        set_quota_holding_time(timerConfig, 6);
+        clear_operational_flag(timerConfig, OPERATIONAL_FLAG_ZB12_STOPPED); // this should be the default anyway
+        set_last_pkt_time(timerConfig, now_sec);
         SECTION("ZB12 not modified"){
-            how_long_until_the_next_alarm(alarmConfig, now_sec, &min_value_ms);
+            how_long_until_next_timer_expiry(timerConfig, now_sec, &min_value_ms);
             REQUIRE(min_value_ms == 6000);
-            REQUIRE(!get_operational_flag_state(alarmConfig, OPERATIONAL_FLAG_ZB12_MODIFIED));
+            REQUIRE(!get_operational_flag_state(timerConfig, OPERATIONAL_FLAG_ZB12_MODIFIED));
         }
         SECTION("ZB12 modified") {
-            add_operational_flag(alarmConfig, OPERATIONAL_FLAG_ZB12_MODIFIED);
-            how_long_until_the_next_alarm(alarmConfig, now_sec, &min_value_ms);
+            add_operational_flag(timerConfig, OPERATIONAL_FLAG_ZB12_MODIFIED);
+            how_long_until_next_timer_expiry(timerConfig, now_sec, &min_value_ms);
             REQUIRE(min_value_ms == 6000);
-            REQUIRE(!get_operational_flag_state(alarmConfig, OPERATIONAL_FLAG_ZB12_MODIFIED));
+            REQUIRE(!get_operational_flag_state(timerConfig, OPERATIONAL_FLAG_ZB12_MODIFIED));
 
         }
 
     }
-    SECTION("DY9X alarm") {
-        add_reporting_flag(alarmConfig, REPORTING_TRIGGERS_DY9X);
-        set_meas_dy9xd(alarmConfig, 5);
-        set_periodic_meas_start(alarmConfig, now_sec - 2);
-        how_long_until_the_next_alarm(alarmConfig, now_sec, &min_value_ms);
+    SECTION("DY9X timer") {
+        add_reporting_flag(timerConfig, REPORTING_TRIGGERS_DY9X);
+        set_meas_dy9xd(timerConfig, 5);
+        set_periodic_meas_start(timerConfig, now_sec - 2);
+        how_long_until_next_timer_expiry(timerConfig, now_sec, &min_value_ms);
         REQUIRE(min_value_ms == 3000);
     }
-    SECTION("Monitoring time TS") {
-        set_monitoring_time_ts(alarmConfig, 7);
-        set_monitoring_time_start(alarmConfig, now_sec - 4);
-        how_long_until_the_next_alarm(alarmConfig, now_sec, &min_value_ms);
+    SECTION("Monitoring time timer") {
+        set_monitoring_time_ts(timerConfig, 7);
+        set_monitoring_time_start(timerConfig, now_sec - 4);
+        how_long_until_next_timer_expiry(timerConfig, now_sec, &min_value_ms);
         REQUIRE(min_value_ms == 3000);
     }
 
-    SECTION("All alarms set") {
-        set_duration_meas_active(alarmConfig, true);
-        set_duration_meas_start(alarmConfig, now_sec - 1);
-        set_last_pkt_time(alarmConfig, now_sec);
-        set_idt_alarm_time(alarmConfig, 30);
+    SECTION("All timers set") {
+        set_duration_meas_active(timerConfig, true);
+        set_duration_meas_start(timerConfig, now_sec - 1);
+        set_last_pkt_time(timerConfig, now_sec);
+        set_idt_alarm_time(timerConfig, 30);
 
-        add_reporting_flag(alarmConfig, REPORTING_TRIGGERS_P88N);
-        set_time_threshold(alarmConfig, 26);
+        add_reporting_flag(timerConfig, REPORTING_TRIGGERS_P88N);
+        set_time_threshold(timerConfig, 26);
 
-        add_operational_flag(alarmConfig, OPERATIONAL_FLAG_TIME_QUOTA_PRESENT);
-        set_time_quota(alarmConfig, 21);
+        add_operational_flag(timerConfig, OPERATIONAL_FLAG_TIME_QUOTA_PRESENT);
+        set_time_quota(timerConfig, 21);
 
-        add_reporting_flag(alarmConfig, REPORTING_TRIGGERS_ZB12);
-        set_quota_holding_time(alarmConfig, 17);
-        clear_operational_flag(alarmConfig, OPERATIONAL_FLAG_ZB12_STOPPED); // this should be the default anyway
-        add_operational_flag(alarmConfig, OPERATIONAL_FLAG_ZB12_MODIFIED);
+        add_reporting_flag(timerConfig, REPORTING_TRIGGERS_ZB12);
+        set_quota_holding_time(timerConfig, 17);
+        clear_operational_flag(timerConfig, OPERATIONAL_FLAG_ZB12_STOPPED); // this should be the default anyway
+        add_operational_flag(timerConfig, OPERATIONAL_FLAG_ZB12_MODIFIED);
 
-        add_reporting_flag(alarmConfig, REPORTING_TRIGGERS_DY9X);
-        set_meas_dy9xd(alarmConfig, 15);
-        set_periodic_meas_start(alarmConfig, now_sec - 2);
+        add_reporting_flag(timerConfig, REPORTING_TRIGGERS_DY9X);
+        set_meas_dy9xd(timerConfig, 15);
+        set_periodic_meas_start(timerConfig, now_sec - 2);
 
-        set_monitoring_time_ts(alarmConfig, 7);
-        set_monitoring_time_start(alarmConfig, now_sec - 4);
+        set_monitoring_time_ts(timerConfig, 7);
+        set_monitoring_time_start(timerConfig, now_sec - 4);
 
-        how_long_until_the_next_alarm(alarmConfig, now_sec, &min_value_ms);
+        how_long_until_next_timer_expiry(timerConfig, now_sec, &min_value_ms);
         REQUIRE(min_value_ms == 3000);
     }
 }
