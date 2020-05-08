8e75654310c34b366c8772301cd1826b4bfca21f
emily <emily@bacheconsulting.com>
2020-05-08 16:12:46 +0200

    full coverage with approval tests

--- c/test/catch2_approvaltest/TimerExpiryCatch2ApprovalTests.cpp
+++ c/test/catch2_approvaltest/TimerExpiryCatch2ApprovalTests.cpp
@@ -10,6 +10,7 @@ extern "C"
 #include "timers.h"
 }
 
+
 /*
  * This is a printer for a timer_config instance.
  * It generally prints fields that are not zero.
@@ -45,8 +46,21 @@ std::ostream &operator<<(std::ostream &os, const struct timer_config &obj) {
     return os;
 }
 
-TEST_CASE ("TimerExpiry", "how_long_until_next_timer_expiry")
-{
+std::string do_next_timer_expiry(struct timer_config *timerConfig, unsigned int now_sec, unsigned long min_value_ms) {
+    std::stringstream to_approve;
+    to_approve << "\nBEFORE:\n" << *timerConfig;
+    how_long_until_next_timer_expiry(timerConfig, now_sec, &min_value_ms);
+    to_approve << "\nAFTER:\n" << *timerConfig;
+    to_approve << "\nNEXT_TIMER: ";
+    if (min_value_ms != LONG_MAX) {
+        to_approve << min_value_ms;
+    } else {
+        to_approve << "LONG_MAX";
+    }
+    return to_approve.str();
+}
+
+TEST_CASE ("TimerExpiry", "how_long_until_next_timer_expiry") {
     // seconds since the epoch
     unsigned int now_sec = 100;
     // in-parameter for when the next timer will expire, in milliseconds after now_sec
@@ -57,45 +71,48 @@ TEST_CASE ("TimerExpiry", "how_long_until_next_timer_expiry")
     timerConfig->timers = new timers();
     timerConfig->timers->duration = new duration();
 
-    set_duration_meas_active(timerConfig, true);
-    set_duration_meas_start(timerConfig, now_sec - 1);
-    set_last_pkt_time(timerConfig, now_sec);
-    set_idt_alarm_time(timerConfig, 30);
+    SECTION("no timers set") {
+        auto to_approve = do_next_timer_expiry(timerConfig, now_sec, min_value_ms);
+        ApprovalTests::Approvals::verify(to_approve);
+    }
 
-    add_reporting_flag(timerConfig, REPORTING_TRIGGERS_P88N);
-    set_time_threshold(timerConfig, 26);
-
-    add_operational_flag(timerConfig, OPERATIONAL_FLAG_TIME_QUOTA_PRESENT);
-    set_time_quota(timerConfig, 21);
-
-    add_reporting_flag(timerConfig, REPORTING_TRIGGERS_ZB12);
-    set_quota_holding_time(timerConfig, 17);
-    clear_operational_flag(timerConfig, OPERATIONAL_FLAG_ZB12_STOPPED); // this should be the default anyway
-    add_operational_flag(timerConfig, OPERATIONAL_FLAG_ZB12_MODIFIED);
-
-    add_reporting_flag(timerConfig, REPORTING_TRIGGERS_DY9X);
-    set_meas_dy9xd(timerConfig, 15);
-    set_periodic_meas_start(timerConfig, now_sec - 2);
-
-    set_monitoring_time_ts(timerConfig, 7);
-    set_monitoring_time_start(timerConfig, now_sec - 4);
+    SECTION("ZB12 modified") {
+        add_reporting_flag(timerConfig, REPORTING_TRIGGERS_ZB12);
+        set_quota_holding_time(timerConfig, 7);
+        clear_operational_flag(timerConfig, OPERATIONAL_FLAG_ZB12_STOPPED);
+        add_operational_flag(timerConfig, OPERATIONAL_FLAG_ZB12_MODIFIED);
+        auto to_approve = do_next_timer_expiry(timerConfig, now_sec, min_value_ms);
+        ApprovalTests::Approvals::verify(to_approve);
+    }
 
     SECTION("All timers set") {
-        how_long_until_next_timer_expiry(timerConfig, now_sec, &min_value_ms);
-        std::stringstream to_approve;
-        to_approve << *timerConfig;
-        to_approve << "NEXT_TIMER: ";
+        set_duration_meas_active(timerConfig, true);
+        set_duration_meas_start(timerConfig, now_sec - 1);
+        set_last_pkt_time(timerConfig, now_sec);
+        set_idt_alarm_time(timerConfig, 30);
 
-        if (min_value_ms != LONG_MAX) {
-            to_approve << min_value_ms;
-        } else {
-            to_approve << "LONG_MAX";
-        }
+        add_reporting_flag(timerConfig, REPORTING_TRIGGERS_P88N);
+        set_time_threshold(timerConfig, 26);
 
-        auto t = to_approve.str();
-        ApprovalTests::Approvals::verify(t);
+        add_operational_flag(timerConfig, OPERATIONAL_FLAG_TIME_QUOTA_PRESENT);
+        set_time_quota(timerConfig, 21);
 
+        add_reporting_flag(timerConfig, REPORTING_TRIGGERS_ZB12);
+        set_quota_holding_time(timerConfig, 17);
+        clear_operational_flag(timerConfig, OPERATIONAL_FLAG_ZB12_STOPPED); // this should be the default anyway
+
+        add_reporting_flag(timerConfig, REPORTING_TRIGGERS_DY9X);
+        set_meas_dy9xd(timerConfig, 15);
+        set_periodic_meas_start(timerConfig, now_sec - 2);
+
+        set_monitoring_time_ts(timerConfig, 7);
+        set_monitoring_time_start(timerConfig, now_sec - 4);
+
+        auto to_approve = do_next_timer_expiry(timerConfig, now_sec, min_value_ms);
+        ApprovalTests::Approvals::verify(to_approve);
     }
+
+
 }
 
 
