daff9154ed29b021d0b3a985cf6825c0fb02f8b5
emily <emily@bacheconsulting.com>
2020-05-08 15:48:52 +0200

    first test with fairly good coverage

--- c/test/catch2_approvaltest/TimerExpiryCatch2ApprovalTests.cpp
+++ c/test/catch2_approvaltest/TimerExpiryCatch2ApprovalTests.cpp
@@ -1,4 +1,5 @@
 #define APPROVALS_CATCH
+
 #include "ApprovalTests.hpp"
 
 #include <climits>
@@ -42,11 +43,10 @@ std::ostream &operator<<(std::ostream &os, const struct timer_config &obj) {
         os << "monitoring time ts: " << get_monitoring_time_ts(timerConfig) << "\n";
     }
     return os;
-
-
 }
 
-TEST_CASE ("TimerExpiry", "how_long_until_next_timer_expiry") {
+TEST_CASE ("TimerExpiry", "how_long_until_next_timer_expiry")
+{
     // seconds since the epoch
     unsigned int now_sec = 100;
     // in-parameter for when the next timer will expire, in milliseconds after now_sec
@@ -57,19 +57,45 @@ TEST_CASE ("TimerExpiry", "how_long_until_next_timer_expiry") {
     timerConfig->timers = new timers();
     timerConfig->timers->duration = new duration();
 
-    how_long_until_next_timer_expiry(timerConfig, now_sec, &min_value_ms);
+    set_duration_meas_active(timerConfig, true);
+    set_duration_meas_start(timerConfig, now_sec - 1);
+    set_last_pkt_time(timerConfig, now_sec);
+    set_idt_alarm_time(timerConfig, 30);
 
-    std::stringstream to_approve;
-    to_approve << *timerConfig;
-    to_approve << "NEXT_TIMER: ";
+    add_reporting_flag(timerConfig, REPORTING_TRIGGERS_P88N);
+    set_time_threshold(timerConfig, 26);
 
-    if (min_value_ms != LONG_MAX) {
-        to_approve << min_value_ms;
-    } else {
-        to_approve << "LONG_MAX";
+    add_operational_flag(timerConfig, OPERATIONAL_FLAG_TIME_QUOTA_PRESENT);
+    set_time_quota(timerConfig, 21);
+
+    add_reporting_flag(timerConfig, REPORTING_TRIGGERS_ZB12);
+    set_quota_holding_time(timerConfig, 17);
+    clear_operational_flag(timerConfig, OPERATIONAL_FLAG_ZB12_STOPPED); // this should be the default anyway
+    add_operational_flag(timerConfig, OPERATIONAL_FLAG_ZB12_MODIFIED);
+
+    add_reporting_flag(timerConfig, REPORTING_TRIGGERS_DY9X);
+    set_meas_dy9xd(timerConfig, 15);
+    set_periodic_meas_start(timerConfig, now_sec - 2);
+
+    set_monitoring_time_ts(timerConfig, 7);
+    set_monitoring_time_start(timerConfig, now_sec - 4);
+
+    SECTION("All timers set") {
+        how_long_until_next_timer_expiry(timerConfig, now_sec, &min_value_ms);
+        std::stringstream to_approve;
+        to_approve << *timerConfig;
+        to_approve << "NEXT_TIMER: ";
+
+        if (min_value_ms != LONG_MAX) {
+            to_approve << min_value_ms;
+        } else {
+            to_approve << "LONG_MAX";
+        }
+
+        auto t = to_approve.str();
+        ApprovalTests::Approvals::verify(t);
+
     }
-
-    auto t = to_approve.str();
-    ApprovalTests::Approvals::verify(t);
 }
+
 
