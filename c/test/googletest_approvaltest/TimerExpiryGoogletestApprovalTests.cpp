88dd54fbe81de02c25e8ec0fefd4716190876240
emily <emily@bacheconsulting.com>
2020-10-14 08:41:42 +0200

    approval tests even with gtest

--- c/test/googletest_approvaltest/TimerExpiryGoogletestApprovalTests.cpp
+++ c/test/googletest_approvaltest/TimerExpiryGoogletestApprovalTests.cpp
@@ -1,10 +1,7 @@
-#define APPROVALS_CATCH
+#define APPROVALS_GOOGLETEST
 
+#include <gtest/gtest.h>
 #include "ApprovalTests.hpp"
-
-#include <climits>
-#include <string>
-
 #include "printers.h"
 
 extern "C"
@@ -12,18 +9,28 @@ extern "C"
 #include "timers.h"
 }
 
+class TimerExpiryTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        timerConfig = new timer_config();
+        timerConfig->timers = new timers();
+        timerConfig->timers->duration = new duration();
+    }
 
-TEST_CASE ("TimerExpiry", "how_long_until_next_timer_expiry") {
+    void TearDown() override {
+        delete timerConfig;
+    }
+
+    // the timer configuration for this test case
+    timer_config *timerConfig;
     // seconds since the epoch
     unsigned int now_sec = 100;
     // in-parameter for when the next timer will expire, in milliseconds after now_sec
     unsigned long min_value_ms = LONG_MAX;
 
-    // the configuration of all the various timers
-    auto *timerConfig = new timer_config();
-    timerConfig->timers = new timers();
-    timerConfig->timers->duration = new duration();
+};
 
+TEST_F(TimerExpiryTest, NoTimersSet) {
     how_long_until_next_timer_expiry(timerConfig, now_sec, &min_value_ms);
 
     std::stringstream to_approve;
@@ -39,4 +46,3 @@ TEST_CASE ("TimerExpiry", "how_long_until_next_timer_expiry") {
     auto t = to_approve.str();
     ApprovalTests::Approvals::verify(t);
 }
-
