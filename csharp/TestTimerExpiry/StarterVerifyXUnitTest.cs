01f4f7056cc65acddd1091803593bc462813d88f
emily <emily@bacheconsulting.com>
2023-11-26 11:56:24 +0100

    initial translation to C#

---
+++ csharp/TestTimerExpiry/StarterVerifyXUnitTest.cs
@@ -0,0 +1,26 @@
+using Verifier = VerifyXunit.Verifier;
+
+namespace TestTimerExpiry;
+
+[UsesVerify]
+public class TimerExpiryVerifyXUnitTest
+{
+    [Fact]
+    public Task HowLongUntilNextTimerExpiry()
+    {
+        // seconds since the epoch
+        uint now_sec = 100;
+        // in-parameter for when the next timer will expire, in milliseconds after now_sec
+        ulong min_value_ms = Int64.MaxValue;
+
+        // the configuration of all the various timers
+        var timerConfig = new TimerConfig();
+        timerConfig.Timers = new Timers();
+        timerConfig.Timers.Duration = new Duration();
+
+        timerConfig.how_long_until_next_timer_expiry(timerConfig, now_sec, out min_value_ms);
+
+        // no timers are set, so min_value_ms is not updated and remains at the value we set earlier
+        return Verifier.Verify(min_value_ms);
+    }
+}
---
+++ csharp/TimerExpiry/Timers.cs
@@ -0,0 +1,11 @@
+namespace TestTimerExpiry;
+
+public class Timers
+{
+    public Duration Duration { get; set; }
+    public uint QuotaHoldingTime { get; set; }
+    public uint MonitoringTimeTs { get; set; }
+    public uint PeriodicMeasStart { get; set; }
+    public uint MonitoringTimeStart { get; set; }
+    public uint MeasDy9xd { get; set; }
+}
